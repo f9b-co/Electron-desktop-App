a296d338d9ad1c9217b7119db66aae160a2b9d6c
Fabien Massucco <f9bm9s@gmail.com>
2021-03-18 17:15:09 +0100

    display fixes

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ async function doFetch(endpoint) {
         const resp = await fetch("https://restcountries.eu/rest/v2/" + endpoint, {Method: 'GET', Cache: 'no-store'});
         console.log("https://restcountries.eu/rest/v2/" + endpoint)
         if (resp.ok) {
-            return await resp.text();
+            return await resp.json();
         } else {
             return `error fetching endpoint: /${endpoint}`;
         }
@@ -16,28 +16,37 @@ async function doFetch(endpoint) {
 
 function display(data, selector) {
     const container = document.querySelector(selector);
-    container.innerHTML = "Return order = " + returnOrder + "<p>" + data + "</p>";
+    container.innerHTML = "Return order= " + returnOrder + "</br>";
+    data.forEach(e => {container.innerHTML += "<span>| " + e + " |</span>"});
     container.classList.add('be-bold');
     returnOrder++;
 }
 
 async function fetchA() {
-    const data = await doFetch('regionalbloc/eu?fields=name'); //?fields=name;capital
+    const resp = await doFetch('regionalbloc/eu?fields=name;capital');
+    const data = [];
+    resp.forEach(o => data.push(o.name+"/"+o.capital));
     display(data, '#component-a');
 }
 
 async function fetchB(name) {
-    const data = await doFetch('name/'+name+'?fields=name;alpha2Code;region');
+    const resp = await doFetch('name/'+name+'?fields=name;alpha2Code;region');
+    const data = [];
+    resp.forEach(o => data.push(o.name+"/"+o.alpha2Code+"/"+o.region));
     display(data, '#component-b');
 }
 
 async function fetchC(capital) {
-    const data = await doFetch('capital/'+capital+'?fields=name');
+    const resp = await doFetch('capital/'+capital+'?fields=name');
+    const data = [];
+    resp.forEach(o => data.push(o.name));
     display(data, '#component-c');
 }
 
 async function fetchD(et) {
-    const data = await doFetch('lang/'+et+'?fields=name');
+    const resp = await doFetch('lang/'+et+'?fields=name');
+    const data = [];
+    resp.forEach(o => data.push(o.name));
     display(data, '#component-d');
 }
 
